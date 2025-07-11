dc4b26e2802e87f4579f7e79159ea79b00e586a9
Anup Gupta <agupta-contractor@ambrygen.com>
2025-07-11 22:36:36 +0530

    Initialize Spring Boot application with basic configuration and test setup

---
+++ src/test/java/com/nonstop/lld_impl/LldImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonstop.lld_impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LldImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
